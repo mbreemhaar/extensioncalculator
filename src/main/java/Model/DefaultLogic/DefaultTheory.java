fc2a932eee2b30c65beeecd9aa53d14aca888fcc
Marco Breemhaar <marcobreemhaar@gmail.com>
2018-05-27 17:14:58 +0200

    Added add/remove methods to default theory class

--- src/main/java/Model/DefaultLogic/DefaultTheory.java
+++ src/main/java/Model/DefaultLogic/DefaultTheory.java
@@ -2,6 +2,8 @@ package Model.DefaultLogic;
 
 import Model.PropositionalLogic.Formula;
 
+import java.util.HashSet;
+import java.util.Observable;
 import java.util.Set;
 
 /**
@@ -9,6 +11,20 @@ import java.util.Set;
  */
 
 public class DefaultTheory {
-    private Set<Formula> axioms;
-    private Set<Formula> defaults;
+    private Set<Formula> axioms = new HashSet<>();
+    private Set<Default> defaults = new HashSet<>();
+
+    public static DefaultTheory currentTheory;
+
+    public DefaultTheory() {
+        currentTheory = this;
+    }
+
+    public void add(Formula f) {
+        axioms.add(f);
+    }
+
+    public void add(Default d) {
+        defaults.add(d);
+    }
 }
